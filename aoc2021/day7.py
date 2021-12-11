9b6193148c43de3dedfe1e8e49a7130c80447c65
Maxime Mouchet <max@maxmouchet.com>
2021-12-11 16:17:24 +0100

    Day 7

---
+++ aoc2021/day7.py
@@ -0,0 +1,39 @@
+import math
+
+
+def evaluate(positions, target, *, weighted):
+    """
+    >>> positions = [16, 1, 2, 0, 4, 2, 7, 1, 2, 14]
+    >>> evaluate(positions, 1, weighted=False)
+    41
+    >>> evaluate(positions, 2, weighted=False)
+    37
+    >>> evaluate(positions, 10, weighted=False)
+    71
+    >>> evaluate(positions, 5, weighted=True)
+    168
+    """
+    f = lambda x: x
+    if weighted:
+        f = lambda x: x * (x + 1) // 2
+    return sum(f(abs(position - target)) for position in positions)
+
+
+def minimize(positions, *, weighted):
+    best = math.inf
+    for i in range(min(positions), max(positions)):
+        candidate = evaluate(positions, i, weighted=weighted)
+        if candidate > best:
+            return best
+        best = candidate
+
+
+def main():
+    with open("inputs/day7.txt") as f:
+        positions = [int(x) for x in next(f).split(",")]
+    print(minimize(positions, weighted=False))
+    print(minimize(positions, weighted=True))
+
+
+if __name__ == "__main__":
+    main()
